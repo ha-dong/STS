5eb1d51b3022151f8df5e765743c63fa022aaa86
jeong jaehun <145264424+ha-dong@users.noreply.github.com>
2024-08-09 10:55:11 +0900

    Add files via upload

---
+++ springrest/src/main/java/springrest/dao/RestBoardDaoImpl.java
@@ -0,0 +1,42 @@
+package springrest.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import springrest.domain.RestBoard;
+
+@Repository
+public class RestBoardDaoImpl implements RestBoardDao {
+	
+	@Autowired
+	private SqlSession sqlSession;
+
+	@Override
+	public List<RestBoard> listRestBoard() throws Exception {
+		return sqlSession.selectList("RestBoard.listRestBoard");
+	}
+
+	@Override
+	public RestBoard getRestBoard(int rbid) throws Exception {
+		return sqlSession.selectOne("RestBoard.getRestBoard", rbid);
+	}
+
+	@Override
+	public int insertRestBoard(RestBoard restBoard) throws Exception {
+		return sqlSession.insert("RestBoard.insertRestBoard", restBoard);
+	}
+
+	@Override
+	public int updateRestBoard(RestBoard restBoard) throws Exception {
+		return sqlSession.update("RestBoard.updateRestBoard", restBoard);
+	}
+
+	@Override
+	public int deleteRestBoard(int rbid) throws Exception {
+		return sqlSession.delete("RestBoard.getRestBoard", rbid);
+	}
+
+}
---
+++ springrest/src/main/java/springrest/test/RestBoardTest.java
@@ -0,0 +1,79 @@
+package springrest.test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import springrest.domain.RestBoard;
+import springrest.service.RestBoardService;
+import springrest.service.RestBoardServiceImpl;
+
+public class RestBoardTest {
+	
+	private ApplicationContext context;
+	
+	private RestBoardService restBoardService;
+
+	@Before
+	public void init() {
+		context = new GenericXmlApplicationContext("springrest/conf/beans.xml");
+		restBoardService = (RestBoardServiceImpl)context.getBean("restBoardService");
+	}
+	
+//	@Test
+	public void insertRestBoard() throws Exception {
+		RestBoard restBoard = new RestBoard(0, "작성자", "제목", "내용", null);
+		int result = restBoardService.insertRestBoard(restBoard);
+		assertEquals(result, 1);
+	}
+	
+//	@Test
+	public void selectRestBoard()throws Exception{
+		List<RestBoard> restBoardList=restBoardService.listRestBoard();
+		System.out.println(restBoardList);
+	}
+	
+//	@Test
+	public void getRestBoard()throws Exception{
+		RestBoard restBoard = restBoardService.getRestBoard(3);
+		System.out.println(restBoard);
+	}
+	
+//	@Test
+	public void updateRestBoard() throws Exception{
+		RestBoard restBoard=new RestBoard(3, "작성자up", "제목up", "내용up", null);
+		int result=restBoardService.updateRestBoard(restBoard);
+		assertEquals(result, 1);
+	}
+	
+	@Test
+	public void deleteRestBoard() throws Exception{
+		int result=restBoardService.deleteRestBoard(5);
+		assertEquals(result, 1);
+	}
+
+} // class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
