c845ebd862c993b65c9e397a42c8cf1b5cd408e0
jeong jaehun <145264424+ha-dong@users.noreply.github.com>
2024-09-25 17:26:53 +0900

    Add files via upload

--- StoryCraft/src/main/java/com/example/storycraft/dao/SceneDao.java
+++ StoryCraft/src/main/java/com/example/storycraft/dao/SceneDao.java
@@ -3,7 +3,6 @@ package com.example.storycraft.dao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-
 import com.example.storycraft.model.Scene;
 
 import java.sql.ResultSet;
@@ -16,53 +15,99 @@ public class SceneDao {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    // 씬 저장
+    /**
+     * 씬 저장
+     */
     public void saveScene(Scene scene) {
-        String sql = "INSERT INTO SCENE (SC_NUM, PLAYER_NUM, SC_TEXT, SC_CHOICES, MONEY, HP, END_CODE, SOUND_CODE) " +
-                     "VALUES (SCENE_SEQ.NEXTVAL, ?, ?, ?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO SCENE (SC_NUM, ST_NUM, PARENT_SC_NUM, SC_LEVEL, PLAYER_NUM, SC_TEXT, SC_CHOICES, SC_ILLUS, MONEY, HP, END_CODE, SOUND_CODE) " +
+                     "VALUES (SEQ_SCENE.NEXTVAL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-        jdbcTemplate.update(sql, scene.getPlayerNum(), scene.getScText(), scene.getScChoices(),
-                scene.getMoney(), scene.getHp(), scene.getEndCode(), scene.getSoundCode());
+        jdbcTemplate.update(sql,
+                scene.getStNum(),
+                scene.getParentScNum(),
+                scene.getScLevel(),
+                scene.getPlayerNum(),
+                scene.getScText(),
+                scene.getScChoices(),
+                scene.getScIllus(),
+                scene.getMoney(),
+                scene.getHp(),
+                scene.getEndCode(),
+                scene.getSoundCode()
+        );
     }
 
-    // 씬 ID로 특정 씬 조회
+    /**
+     * 씬 ID로 특정 씬 조회
+     */
     public Scene findSceneById(Long scNum) {
         String sql = "SELECT * FROM SCENE WHERE SC_NUM = ?";
-        return jdbcTemplate.queryForObject(sql, new Object[]{scNum}, this::mapRowToScene);
+        return jdbcTemplate.queryForObject(sql, new Object[]{scNum}, (rs, rowNum) -> {
+            Scene scene = new Scene();
+            scene.setScNum(rs.getLong("SC_NUM"));
+            scene.setStNum(rs.getLong("ST_NUM"));
+            scene.setParentScNum(rs.getLong("PARENT_SC_NUM"));
+            scene.setScLevel(rs.getLong("SC_LEVEL"));
+            scene.setPlayerNum(rs.getLong("PLAYER_NUM"));
+            scene.setScText(rs.getString("SC_TEXT"));
+            scene.setScChoices(rs.getString("SC_CHOICES"));
+            scene.setScIllus(rs.getString("SC_ILLUS"));
+            scene.setMoney(rs.getLong("MONEY"));
+            scene.setHp(rs.getLong("HP"));
+            scene.setEndCode(rs.getString("END_CODE"));
+            scene.setSoundCode(rs.getString("SOUND_CODE"));
+            return scene;
+        });
     }
 
-    // 모든 씬 조회
-    public List<Scene> findAllScenes() {
-        String sql = "SELECT * FROM SCENE";
-        return jdbcTemplate.query(sql, this::mapRowToScene);
+    /**
+     * 스토리에 속한 모든 씬 조회
+     */
+    public List<Scene> findAllScenesByStory(Long stNum) {
+        String sql = "SELECT * FROM SCENE WHERE ST_NUM = ?";
+        return jdbcTemplate.query(sql, new Object[]{stNum}, (rs, rowNum) -> {
+            Scene scene = new Scene();
+            scene.setScNum(rs.getLong("SC_NUM"));
+            scene.setStNum(rs.getLong("ST_NUM"));
+            scene.setParentScNum(rs.getLong("PARENT_SC_NUM"));
+            scene.setScLevel(rs.getLong("SC_LEVEL"));
+            scene.setPlayerNum(rs.getLong("PLAYER_NUM"));
+            scene.setScText(rs.getString("SC_TEXT"));
+            scene.setScChoices(rs.getString("SC_CHOICES"));
+            scene.setScIllus(rs.getString("SC_ILLUS"));
+            scene.setMoney(rs.getLong("MONEY"));
+            scene.setHp(rs.getLong("HP"));
+            scene.setEndCode(rs.getString("END_CODE"));
+            scene.setSoundCode(rs.getString("SOUND_CODE"));
+            return scene;
+        });
     }
 
-    // 씬 업데이트
+    /**
+     * 씬 업데이트
+     */
     public void updateScene(Scene scene) {
-        String sql = "UPDATE SCENE SET PLAYER_NUM = ?, SC_TEXT = ?, SC_CHOICES = ?, MONEY = ?, HP = ?, END_CODE = ?, SOUND_CODE = ? " +
+        String sql = "UPDATE SCENE SET SC_TEXT = ?, SC_CHOICES = ?, SC_ILLUS = ?, MONEY = ?, HP = ?, END_CODE = ?, SOUND_CODE = ? " +
                      "WHERE SC_NUM = ?";
 
-        jdbcTemplate.update(sql, scene.getPlayerNum(), scene.getScText(), scene.getScChoices(),
-                scene.getMoney(), scene.getHp(), scene.getEndCode(), scene.getSoundCode(), scene.getScNum());
+        jdbcTemplate.update(sql,
+                scene.getScText(),
+                scene.getScChoices(),
+                scene.getScIllus(),
+                scene.getMoney(),
+                scene.getHp(),
+                scene.getEndCode(),
+                scene.getSoundCode(),
+                scene.getScNum()
+        );
     }
 
-    // 씬 삭제
-    public void deleteScene(Long scNum) {
+    /**
+     * 씬 삭제
+     */
+    public boolean deleteScene(Long scNum) {
         String sql = "DELETE FROM SCENE WHERE SC_NUM = ?";
-        jdbcTemplate.update(sql, scNum);
-    }
-
-    // RowMapper 함수: ResultSet을 Scene 객체로 변환
-    private Scene mapRowToScene(ResultSet rs, int rowNum) throws SQLException {
-        Scene scene = new Scene();
-        scene.setScNum(rs.getLong("SC_NUM"));
-        scene.setPlayerNum(rs.getLong("PLAYER_NUM"));
-        scene.setScText(rs.getString("SC_TEXT"));
-        scene.setScChoices(rs.getString("SC_CHOICES"));
-        scene.setMoney(rs.getInt("MONEY"));
-        scene.setHp(rs.getInt("HP"));
-        scene.setEndCode(rs.getString("END_CODE"));
-        scene.setSoundCode(rs.getString("SOUND_CODE"));
-        return scene;
+        int rows = jdbcTemplate.update(sql, scNum);
+        return rows > 0;
     }
 }
--- StoryCraft/src/main/java/com/example/storycraft/dao/StoryDao.java
+++ StoryCraft/src/main/java/com/example/storycraft/dao/StoryDao.java
@@ -2,8 +2,10 @@ package com.example.storycraft.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import com.example.storycraft.model.Choice;
 import com.example.storycraft.model.Story;
 
 import java.sql.ResultSet;
@@ -17,58 +19,143 @@ public class StoryDao {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    // 스토리 저장
+    // 스토리 저장 메서드
     public void saveStory(Story story) {
-        String sql = "INSERT INTO STORY (ST_NUM, ST_TITLE, ST_CRDATE, ST_TYPECODE, ST_GENRECODE, U_ID) " +
-                     "VALUES (STORY_SEQ.NEXTVAL, ?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO STORY (ST_NUM, ST_TITLE, ST_CRDATE, ST_EDDATE, ST_VIEWNUM, ST_TYPECODE, ST_GENRECODE, ST_SUGNUM, ST_COVER, ST_DSTATUS, ST_DREASON, ST_DDATE, U_ID, ST_CONTENT) " +
+                     "VALUES (STORY_SEQ.NEXTVAL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-        jdbcTemplate.update(sql, story.getStTitle(), new Timestamp(System.currentTimeMillis()),
-                story.getStTypecode(), story.getStGenrecode(), story.getUId());
+        jdbcTemplate.update(sql, 
+            story.getStTitle(),
+            new Timestamp(System.currentTimeMillis()), // ST_CRDATE
+            null, // ST_EDDATE
+            0L, // ST_VIEWNUM 초기값
+            "CST-02", // ST_TYPECODE (회원스토리)
+            mapGenreCode(story.getStGenrecode()), // ST_GENRECODE
+            0L, // ST_SUGNUM 초기값
+            story.getStCover(),
+            "N", // ST_DSTATUS
+            null, // ST_DREASON
+            null, // ST_DDATE
+            story.getuId(),
+            story.getStContent()
+        );
+    }
+
+    // 장르 코드 매핑 메서드
+    private String mapGenreCode(String genre) {
+        switch (genre) {
+            case "판타지":
+                return "CG-01";
+            case "스릴러":
+                return "CG-02";
+            case "코미디":
+                return "CG-03";
+            case "Sci-Fi":
+                return "CG-04";
+            case "미스터리":
+                return "CG-05";
+            case "로맨스":
+                return "CG-06";
+            case "호러":
+                return "CG-07";
+            case "무협":
+                return "CG-08";
+            case "드라마":
+                return "CG-09";
+            case "서부":
+                return "CG-10";
+            case "역사":
+                return "CG-11";
+            default:
+                return "CG-00"; // 기타
+        }
+    }
+
+    // 모든 스토리 목록 조회
+    public List<Story> findAllStories() {
+        String sql = "SELECT * FROM STORY";
+        return jdbcTemplate.query(sql, (rs, rowNum) -> {
+            Story story = new Story();
+            story.setStNum(rs.getLong("ST_NUM"));
+            story.setStTitle(rs.getString("ST_TITLE"));
+            story.setStContent(rs.getString("ST_CONTENT"));
+            story.setStCover(rs.getString("ST_COVER"));
+            story.setuId(rs.getString("U_ID"));
+            story.setStTypecode(rs.getString("ST_TYPECODE"));
+            story.setStGenrecode(rs.getString("ST_GENRECODE"));
+            story.setStSugnum(rs.getLong("ST_SUGNUM"));
+            story.setStCrdate(rs.getTimestamp("ST_CRDATE"));
+            return story;
+        });
     }
 
     // 스토리 ID로 특정 스토리 조회
     public Story findStoryById(Long stNum) {
         String sql = "SELECT * FROM STORY WHERE ST_NUM = ?";
-        return jdbcTemplate.queryForObject(sql, new Object[]{stNum}, this::mapRowToStory);
+        return jdbcTemplate.queryForObject(sql, new Object[]{stNum}, (rs, rowNum) -> {
+            Story story = new Story();
+            story.setStNum(rs.getLong("ST_NUM"));
+            story.setStTitle(rs.getString("ST_TITLE"));
+            story.setStCrdate(rs.getTimestamp("ST_CRDATE"));
+            story.setStEddate(rs.getTimestamp("ST_EDDATE"));
+            story.setStViewnum(rs.getLong("ST_VIEWNUM"));
+            story.setStTypecode(rs.getString("ST_TYPECODE"));
+            story.setStGenrecode(rs.getString("ST_GENRECODE"));
+            story.setStSugnum(rs.getLong("ST_SUGNUM"));
+            story.setStCover(rs.getString("ST_COVER"));
+            story.setStDstatus(rs.getString("ST_DSTATUS"));
+            story.setStDreason(rs.getString("ST_DREASON"));
+            story.setStDdate(rs.getTimestamp("ST_DDATE"));
+            story.setuId(rs.getString("U_ID"));
+            story.setStContent(rs.getString("ST_CONTENT"));
+            return story;
+        });
     }
-
-    // 모든 스토리 조회
-    public List<Story> findAllStories() {
-        String sql = "SELECT * FROM STORY";
-        return jdbcTemplate.query(sql, this::mapRowToStory);
+    
+    // 모든 선택지 목록을 조회하는 메서드
+    public List<Choice> findAllChoices() {
+        String sql = "SELECT * FROM CHOICE"; // CHOICE 테이블 이름이 실제로 다르면 수정 필요
+        return jdbcTemplate.query(sql, new RowMapper<Choice>() {
+            @Override
+            public Choice mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Choice choice = new Choice();
+                choice.setId(rs.getLong("CHOICE_ID")); // 실제 컬럼명으로 수정 필요
+                choice.setName(rs.getString("CHOICE_NAME"));
+                choice.setContent(rs.getString("CHOICE_CONTENT"));
+                choice.setMoneyEffect(rs.getLong("MONEY_EFFECT"));
+                choice.setHpEffect(rs.getLong("HP_EFFECT"));
+                return choice;
+            }
+        });
     }
 
     // 스토리 업데이트
     public void updateStory(Story story) {
-        String sql = "UPDATE STORY SET ST_TITLE = ?, ST_TYPECODE = ?, ST_GENRECODE = ?, ST_CRDATE = ? " +
+        String sql = "UPDATE STORY SET ST_TITLE = ?, ST_EDDATE = ?, ST_VIEWNUM = ?, ST_TYPECODE = ?, " +
+                     "ST_GENRECODE = ?, ST_SUGNUM = ?, ST_COVER = ?, ST_DSTATUS = ?, ST_DREASON = ?, ST_DDATE = ?, U_ID = ?, ST_CONTENT = ? " +
                      "WHERE ST_NUM = ?";
 
-        jdbcTemplate.update(sql, story.getStTitle(), story.getStTypecode(), story.getStGenrecode(),
-                new Timestamp(System.currentTimeMillis()), story.getStNum());
+        jdbcTemplate.update(sql,
+            story.getStTitle(),
+            new Timestamp(System.currentTimeMillis()), // ST_EDDATE
+            story.getStViewnum(),
+            story.getStTypecode(),
+            story.getStGenrecode(),
+            story.getStSugnum(),
+            story.getStCover(),
+            story.getStDstatus(),
+            story.getStDreason(),
+            story.getStDdate(),
+            story.getuId(),
+            story.getStContent(),
+            story.getStNum()
+        );
     }
 
     // 스토리 삭제
-    public void deleteStory(Long stNum) {
+    public boolean deleteStory(Long stNum) {
         String sql = "DELETE FROM STORY WHERE ST_NUM = ?";
-        jdbcTemplate.update(sql, stNum);
-    }
-
-    // 스토리와 씬을 함께 저장
-    public void saveStoryAndScene(Story story) {
-        // 스토리 저장
-        saveStory(story);
-        // 씬 저장 로직 추가 가능 (만약 story와 연결된 씬이 있다면)
-    }
-
-    // RowMapper 함수: ResultSet을 Story 객체로 변환
-    private Story mapRowToStory(ResultSet rs, int rowNum) throws SQLException {
-        Story story = new Story();
-        story.setStNum(rs.getLong("ST_NUM"));
-        story.setStTitle(rs.getString("ST_TITLE"));
-        story.setStCrdate(rs.getTimestamp("ST_CRDATE"));
-        story.setStTypecode(rs.getString("ST_TYPECODE"));
-        story.setStGenrecode(rs.getString("ST_GENRECODE"));
-        story.setUId(rs.getString("U_ID"));
-        return story;
+        int rows = jdbcTemplate.update(sql, stNum);
+        return rows > 0;
     }
 }
